7a1b424becd55ece3ecedd6a4d42964486fec4ea
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-20 21:34:26 +0000

    sync to s3

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
-
-
+set -x
+npm install -g aws-sdk
+set +x
 
 set -x
-echo $PATH
-whereis node
+aws
 set +x
+
+# set -x
+# npm run build && aws s3 sync build/ s3://aviato-team
+# set +x
 
