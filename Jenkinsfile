ef3227a78a4baf940318779bb3185535ffd4f7af
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-20 20:41:13 +0000

    sync to s3

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,14 +29,12 @@ pipeline {
                 sh './jenkins/scripts/kill.sh'
             }
         }
-        stage('Deploy for production') {
+        stage('Deploy to S3 bucket') {
             when {
-                branch 'production'  
+                branch 'production'
             }
             steps {
                 sh './jenkins/scripts/deploy-for-production.sh'
-                input message: 'Finished using the web site? (Click "Proceed" to continue)'
-                sh './jenkins/scripts/kill.sh'
             }
         }
     }
--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -5,35 +5,14 @@ echo 'production in the local "build" directory (i.e. within the appropriate'
 echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
 echo 'in production mode and optimizes the build for the best performance.'
 set -x
-npm run build
+npm install aws-sdk
 set +x
 
-echo 'The following "npm" command downloads and installs the npm serve module'
-echo '(for serving static sites in production environments) to the local'
-echo '"node_modules" directory (i.e. within the appropriate subdirectory of'
-echo '"/var/jenkins_home/workspace/"), which means that this module should not'
-echo 'need to be downloaded after this Pipeline''s initial run for a given'
-echo 'branch.'
-# set -x
-# npm install -g serve
-# sleep 1
-# serve -s build
-# set +x
-
-echo 'The following "serve" command runs the npm serve module (downloaded'
-echo 'above) deploys your Node.js/React application (built above in production'
-echo 'mode) for production and makes the application available for web browsing.'
-echo 'The "serve" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "serve"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "serve") and writes this value to'
-echo 'the file ".pidfile".'
 set -x
-echo $! > .pidfile
+aws configure
 set +x
 
-echo 'Now...'
-echo 'Visit http://localhost:5000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 5000:5000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+set -x
+npm run build && aws s3 sync build/ s3://aviato-team
+set +x
+
