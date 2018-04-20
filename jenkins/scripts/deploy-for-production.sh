dd9b501f42fb6bc3e33403cb55872d5aaced154d
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-20 20:49:43 +0000

    sync to s3

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
-
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the appropriate'
-echo 'subdirectory of "/var/jenkins_home/workspace/"), correctly bundles React'
-echo 'in production mode and optimizes the build for the best performance.'
 set -x
-npm install aws-sdk
+npm install
 set +x
 
 set -x
