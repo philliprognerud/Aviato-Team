f07add11500c87ca09c5b98f3b0c6566ccc3b107
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-21 10:42:13 +0000

    finally done

---
+++ jenkins/scripts/deploy-for-production.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+echo 'Build the React App, and send it to an S3 bucket to deploy'
+echo 'Also start a local server to see the deployed application'
+
+###       Get into the alpine docker image terminal
+####      docker run -it --rm node:8-alpine /bin/ash
+
+
+set -x
+export AWS_ACCESS_KEY_ID=AKIAJNRMOHCG4FLVTRBA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-west-2
+
+apk add --no-cache python3 py3-pip gcc python3-dev py3-cffi    file git curl autoconf automake py3-cryptography linux-headers musl-dev libffi-dev openssl-dev build-base
+pip3 install awscli
+set +x
+
+
+set -x
+npm run build && /usr/bin/aws s3 sync build/ s3://aviato-team
+pwd
+echo ~
+echo $HOME
+echo /home/$USER
+set +x
+
+set -x
+npm install serve
+./node_modules/serve/bin/serve.js -c 0 -s build &
+sleep 1
+echo $! > .pidfile
+set +x
