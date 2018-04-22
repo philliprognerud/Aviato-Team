b902f1c0cd0fe793d025386084cd17b039ecd8ad
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-22 03:11:49 +0000

    finished

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -8,9 +8,9 @@ echo 'Also start a local server to see the deployed application'
 
 
 set -x
-export AWS_ACCESS_KEY_ID=AKIAJNRMOHCG4FLVTRBA
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-2
+# export AWS_ACCESS_KEY_ID
+# export AWS_SECRET_ACCESS_KEY
+# export AWS_DEFAULT_REGION
 
 apk add --no-cache python3 py3-pip gcc python3-dev py3-cffi    file git curl autoconf automake py3-cryptography linux-headers musl-dev libffi-dev openssl-dev build-base
 pip3 install awscli
@@ -19,10 +19,6 @@ set +x
 
 set -x
 npm run build && /usr/bin/aws s3 sync build/ s3://aviato-team
-pwd
-echo ~
-echo $HOME
-echo /home/$USER
 set +x
 
 set -x
