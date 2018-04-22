0e70386278c4efea4f7fc66d083341969df0f7ec
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-22 09:02:20 +0000

    finished

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -6,26 +6,19 @@ echo 'Also start a local server to see the deployed application'
 ###       Get into the alpine docker image terminal
 ####      docker run -it --rm node:8-alpine /bin/ash
 
-
 set -x
-# export AWS_ACCESS_KEY_ID
-# export AWS_SECRET_ACCESS_KEY
-# export AWS_DEFAULT_REGION
-
-# apk add --no-cache python3 py3-pip gcc python3-dev py3-cffi    file git curl autoconf automake py3-cryptography linux-headers musl-dev libffi-dev openssl-dev build-base
-# pip3 install awscli
-echo "My secret is - $AKIAIEMDAF2NRJUQ4KIA"
-echo "My secret is - $MY_SECRET"
+apk add --no-cache python3 py3-pip gcc python3-dev py3-cffi    file git curl autoconf automake py3-cryptography linux-headers musl-dev libffi-dev openssl-dev build-base
+pip3 install awscli
 set +x
 
 
 set -x
-# npm run build && /usr/bin/aws s3 sync build/ s3://aviato-team
+npm run build && /usr/bin/aws s3 sync build/ s3://aviato-team
 set +x
 
 set -x
-# npm install serve
-# ./node_modules/serve/bin/serve.js -c 0 -s build &
-# sleep 1
-# echo $! > .pidfile
+npm install serve
+./node_modules/serve/bin/serve.js -c 0 -s build &
+sleep 1
+echo $! > .pidfile
 set +x
