962f0b2d0c06bf9cf4006e89b20452a3a1726d09
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-24 02:48:09 +0000

    test

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import OktaSignIn from '@okta/okta-signin-widget';
+import React from "react";
+import OktaSignIn from "@okta/okta-signin-widget";
 
-export default class LoginPage extends React.Component{
-  constructor(){
+export default class LoginPage extends React.Component {
+  constructor() {
     super();
     this.state = { user: null };
     this.widget = new OktaSignIn({
-      baseUrl: 'https://dev-842835.oktapreview.com',
-      clientId: '{0oaeszy1axIjhc08c0h7}',
-      redirectUri: 'http://ec2-34-217-105-112.us-west-2.compute.amazonaws.com:3000',
+      baseUrl: "https://dev-842835.oktapreview.com",
+      clientId: "0oaeszy1axIjhc08c0h7",
+      redirectUri:
+        "http://ec2-34-217-105-112.us-west-2.compute.amazonaws.com:3000",
       authParams: {
-        responseType: 'id_token'
+        responseType: "id_token"
       }
     });
 
@@ -18,37 +19,38 @@ export default class LoginPage extends React.Component{
     this.logout = this.logout.bind(this);
   }
 
-  componentDidMount(){
-    this.widget.session.get((response) => {
-      if(response.status !== 'INACTIVE'){
-        this.setState({user:response.login});
-      }else{
+  componentDidMount() {
+    this.widget.session.get(response => {
+      if (response.status !== "INACTIVE") {
+        this.setState({ user: response.login });
+      } else {
         this.showLogin();
       }
     });
   }
 
-  showLogin(){
+  showLogin() {
     // Backbone.history.stop();
-    this.widget.renderEl({el:this.loginContainer},
-      (response) => {
-        this.setState({user: response.claims.email});
+    this.widget.renderEl(
+      { el: this.loginContainer },
+      response => {
+        this.setState({ user: response.claims.email });
       },
-      (err) => {
+      err => {
         console.log(err);
       }
     );
   }
 
-  logout(){
+  logout() {
     this.widget.signOut(() => {
-      this.setState({user: null});
+      this.setState({ user: null });
       this.showLogin();
     });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         {this.state.user ? (
           <div className="container">
@@ -57,7 +59,11 @@ export default class LoginPage extends React.Component{
           </div>
         ) : null}
         {this.state.user ? null : (
-          <div ref={(div) => {this.loginContainer = div; }} />
+          <div
+            ref={div => {
+              this.loginContainer = div;
+            }}
+          />
         )}
       </div>
     );
