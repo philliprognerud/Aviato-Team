c1a95a29fc8d47a70c69f85c3becabbb24679073
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-24 02:18:06 +0000

    update

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import OktaSignIn from '@okta/okta-signin-widget';
 
 export default class LoginPage extends React.Component{
+  constructor(){
+    super();
+    this.state = { user: null };
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://dev-842835.oktapreview.com',
+      clientId: '{0oaeszy1axIjhc08c0h7}',
+      redirectUri: 'http://ec2-34-217-105-112.us-west-2.compute.amazonaws.com:3000',
+      authParams: {
+        responseType: 'id_token'
+      }
+    });
+
+    this.showLogin = this.showLogin.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount(){
+    this.widget.session.get((response) => {
+      if(response.status !== 'INACTIVE'){
+        this.setState({user:response.login});
+      }else{
+        this.showLogin();
+      }
+    });
+  }
+
+  showLogin(){
+    // Backbone.history.stop();
+    this.widget.renderEl({el:this.loginContainer},
+      (response) => {
+        this.setState({user: response.claims.email});
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  logout(){
+    this.widget.signOut(() => {
+      this.setState({user: null});
+      this.showLogin();
+    });
+  }
+
   render(){
     return(
-      <div>Login Page</div>
+      <div>
+        {this.state.user ? (
+          <div className="container">
+            <div>Welcome, {this.state.user}!</div>
+            <button onClick={this.logout}>Logout</button>
+          </div>
+        ) : null}
+        {this.state.user ? null : (
+          <div ref={(div) => {this.loginContainer = div; }} />
+        )}
+      </div>
     );
   }
 }
