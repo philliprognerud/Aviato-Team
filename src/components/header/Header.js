79e8dc09fe494e94f8c01565887f594e59a5f286
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-25 08:59:16 +0000

    test

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,30 @@
 import React from "react";
+import axios from "axios";
 
 export default class Header extends React.Component {
+  async handleClick(e) {
+    let res = await axios.post(
+      "http://ec2-34-217-31-45.us-west-2.compute.amazonaws.com:8000/api/add-okta-user",
+      {
+        firstName: "tim",
+        lastName: "test",
+        email: "philliprognerud35@gmail.com",
+        password: "Deskjet700phil"
+      }
+    );
+
+    console.log(res);
+  }
+
   render() {
     return (
       <div>
         <div class="ui inverted segment">
           <div class="ui inverted secondary pointing menu">
             <a class="active item">Home</a>
-            <a class="item">Messages</a>
+            <a class="item" onClick={e => this.handleClick(e)}>
+              Test POST
+            </a>
             <a class="item" href="/login">
               Login
             </a>
