055da051e85ed58717dfab95246b097ad0e571ea
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-25 19:00:17 +0000

    test

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,12 @@ import React from "react";
 import axios from "axios";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { home: true, login: false, signup: false };
+  }
+
   async handleClick(e) {
     let res = await axios.post(
       "http://ec2-34-217-31-45.us-west-2.compute.amazonaws.com:8000/api/add-okta-user",
@@ -19,15 +25,23 @@ export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <div class="ui inverted segment">
+        <div class="ui inverted segment" style={{ borderRadius: "0px" }}>
           <div class="ui inverted secondary pointing menu">
-            <a class="active item">Home</a>
-            <a class="item" onClick={e => this.handleClick(e)}>
-              Test POST
-            </a>
-            <a class="item" href="/login">
-              Login
-            </a>
+            <a class={`${this.state.home ? "active" : ""} item`}>Home</a>
+            <div class="right menu">
+              <a
+                class={`${this.state.login ? "active" : ""} item`}
+                href="/login"
+              >
+                Login
+              </a>
+              <a
+                class={`${this.state.signup ? "active" : ""} item`}
+                href="/signup"
+              >
+                Signup
+              </a>
+            </div>
           </div>
         </div>
       </div>
