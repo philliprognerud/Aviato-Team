8a29f20bf0228d7c154387c9d1c8d7c3cfc69d9c
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-26 04:27:57 +0000

    test

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ class App extends Component {
 
     this.state = {};
 
-    this.widget = new OktaSignIn({
-      baseUrl: "https://dev-842835.oktapreview.com",
-      clientId: "0oaeszy1axIjhc08c0h7",
-      logo:
-        "http://www.perfectfitcomputers.ca/wp-content/uploads/2014/08/aviato-logo.svg",
-      redirectUri:
-        "http://ec2-34-217-31-45.us-west-2.compute.amazonaws.com:3000",
-      authParams: {
-        responseType: "id_token"
-      }
-    });
+    // this.widget = new OktaSignIn({
+    //   baseUrl: "https://dev-842835.oktapreview.com",
+    //   clientId: "0oaeszy1axIjhc08c0h7",
+    //   logo:
+    //     "http://www.perfectfitcomputers.ca/wp-content/uploads/2014/08/aviato-logo.svg",
+    //   redirectUri:
+    //     "http://ec2-34-217-31-45.us-west-2.compute.amazonaws.com:3000",
+    //   authParams: {
+    //     responseType: "id_token"
+    //   }
+    // });
 
     this.showLogin = this.showLogin.bind(this);
     this.logout = this.logout.bind(this);
