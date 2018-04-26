6afbaf120c04779ca87650ca0686133462cc4a26
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-26 03:30:54 +0000

    test

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -72,6 +72,7 @@ class LoginPage extends React.Component {
   render() {
     return (
       <div>
+        {console.log(this.props.auth)}
         {this.props.auth ? (
           <div className="container">
             <div>Welcome, {this.props.auth.login}!</div>
