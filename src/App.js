41283961678e21a70ab13878342400bd77163329
EC2 Default User <ec2-user@ip-172-31-28-53.us-west-2.compute.internal>
2018-04-27 02:33:25 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "semantic-ui";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "./actions";
